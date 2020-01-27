5b93c560c24843614012c0b98f3e558bc1c63d81
unknown <lucas.leger32@gmail.com>
2020-01-27 17:24:50 +0100

    feat(bo): class block creation

---
+++ WEB/models/block.js
@@ -0,0 +1,14 @@
+
+class Block {
+    constructor(previousId, previousHash, data, hash, creatorid) {
+        this.id = previousId;
+        this.previousHash = previousHash.toString();
+        this.data = {
+            "pagenumber": [6, 7, 8, 9, 10],
+            "text" : ["a", "b", "c", "d", "e"]
+        };
+        this.hash = console.log("fonction de création de hash");
+        this.creatorid = "LUCASS";
+    }
+}
+
