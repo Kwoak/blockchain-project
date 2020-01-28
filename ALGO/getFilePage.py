75cc88feaebc5cb29021bb02bc239409b12d9035
tjuhel <juhel.tais@ynov.com>
2020-01-28 10:51:15 +0100

    feat(bo): getFilePage.py modified

--- ALGO/getFilePage.py
+++ ALGO/getFilePage.py
@@ -1,6 +1,6 @@
 import sys
 
-data = open("trap.txt","r")
+data = open("belAmi.txt","r")
 tbl = data.read().split('\n')
 
 def triPage(r):
@@ -11,23 +11,23 @@ def triPage(r):
     nRow = r+1
     i = 0
     i2 = 0
-    bool = False
+    i3=0
     for row2 in tbl[nRow:len(tbl)]:
-        if bool == False:
-            bool = True
-            i2 += 1
-            debT += row2
-            if i2 - i == 2 :
-                i2 -= 1
-                debT += '  '
-        elif '||' in row2:
-            bool = False
+        i3+=1
+        if '||' in row2:
             i += 1
             numPage = i + int(sys.argv[2])
             debP += str(numPage)
             if i < 5 :
                 debP += ', '
                 debT += '\", \"'
+        else :
+            i2 += 1
+            debT += row2
+            if i2 - i == 2 :
+                i2 -= 1
+                debT += '  '
+            print(debT+" "+str(i3))
         if row2 == str(inputPage)+'||':
             new = open(sys.argv[1], 'w')
             new.write(debP+debT+fin)
@@ -40,5 +40,5 @@ for row in tbl:
     if row == sys.argv[2] + '||':
         triPage(tbl.index(row))
     elif sys.argv[2] == "0":
-        triPage(0)
+        triPage(-1)
 
